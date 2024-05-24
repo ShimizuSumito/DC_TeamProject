29a116d1253f6083cd15e6a1895e63a8bf353e63
DESKTOP-V8QM0UB\sumito.shimizu <sumito.shimizu@g.dreamcareer.co.jp>
2024-05-24 12:18:28 +0900

    Add:SpringBootUpdate

---
+++ SpringBoot/DC_TeamProject/src/main/java/com/example/demo/Repositories/copy/ClothupRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.Controller.Repositories.copy;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClothupRepository extends JpaRepository<Apartment,Integer>  {
+
+}
