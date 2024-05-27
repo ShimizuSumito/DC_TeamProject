f5e139780b91dc3d619487d375e66eca82aa5c71
DESKTOP-V8QM0UB\sumito.shimizu <sumito.shimizu@g.dreamcareer.co.jp>
2024-05-27 13:26:50 +0900

    Add Clothes CRUD

--- SpringBoot/DC_TeamProject/src/main/java/com/example/demo/Controller/CloseupController.java
+++ SpringBoot/DC_TeamProject/src/main/java/com/example/demo/Controller/CloseupController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Data.Clothes;
 import com.example.demo.Data.UserDatabase;
+import com.example.demo.Interface.ClothesInterface;
 import com.example.demo.Interface.UserDatabaseInterface;
 import com.example.demo.Service.ClothupService;
 
@@ -26,6 +27,13 @@ public class CloseupController {
 		return service.GetUserData();
 	}
 	
+	@GetMapping("User/Find/{mailaddress}")
+	@CrossOrigin
+	public UserDatabaseInterface FindUser(@PathVariable String mailaddress)
+	{
+		return service.FindUser(mailaddress);
+	}
+	
 	@PostMapping("User/Add")
 	@CrossOrigin
 	public boolean AddUser(UserDatabase newUser)
@@ -33,7 +41,7 @@ public class CloseupController {
 		return service.AddUser(newUser);
 	}
 
-	@PostMapping("User/delete")
+	@PostMapping("User/Delete")
 	@CrossOrigin
 	public boolean DeleteUser(UserDatabase deleteUser)
 	{
@@ -47,17 +55,38 @@ public class CloseupController {
 		return service.UpdateUser(updateUser);
 	}
 	
-	@GetMapping("User/Find/{mailaddress}")
-	@CrossOrigin
-	public UserDatabaseInterface FindUser(@PathVariable String mailaddress)
-	{
-		return service.FindUser(mailaddress);
-	}
-	
-	@GetMapping("/GetClothes/{mailaddress}")
+	@GetMapping("/Clothes/Get/{mailaddress}")
 	@CrossOrigin
 	public List<Clothes> GetClothes(@PathVariable String mailaddress)
 	{
 		return service.GetClothes(mailaddress);
+	}
+	
+	@GetMapping("/Clothes/Find/{id}")
+	@CrossOrigin
+	public ClothesInterface FindClothes(@PathVariable int id)
+	{
+		return service.FindClothes(id);
+	}
+	
+	@PostMapping("/Clothes/Add")
+	@CrossOrigin
+	public boolean AddClothes(Clothes addClothes)
+	{
+		return service.AddClothes(addClothes);
+	}
+	
+	@PostMapping("/Clothes/Delete")
+	@CrossOrigin
+	public boolean DeleteClothes(Clothes deleteClothes)
+	{
+		return service.DeleteClothes(deleteClothes);
+	}
+	
+	@PostMapping("/Clothes/Update")
+	@CrossOrigin
+	public boolean UpdateClothes(Clothes updateClothes)
+	{
+		return service.UpdateClothes(updateClothes);
 	}
 }
