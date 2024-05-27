9eed1697dcff0ad8fc86f6361091ec4969b0cb6a
yuna <yuna.tamiya@g.dreamcareer.co.jp>
2024-05-27 18:42:50 +0900

    5月27日　新規登録機能書いてます。

--- React/src/components/Start/Register/Check.jsx
+++ React/src/components/Start/Register/Check.jsx
@@ -1,14 +1,60 @@
-
+import React, { useState } from "react";
+import { Value } from "sass";
 import img from "../../../commons/img/girl.jpg";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 export default function Check() {
     const navigate = useNavigate();
-    const toDatabase = () => {
-        navigate('/RegisterSuccess');
-    }
+    const location = useLocation();
+    const [errorMessage, setErrorMessage] = useState('');
+    const [newStock, setNewStock] = useState();
+    
+    const userData = location.state;
+    const formdata = new FormData(userData);
+
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+        const newStock = {
+            user: formdata.get(`name`),
+            mailaddress: formdata.get(`mailaddress`),
+            gender: formdata.get(`gender`),
+            password: formdata.get(`password`),
+            generation:formdata.get(`generation`),
+            password: formdata.get(`password`),
+            region: formdata.get(`region`)
+        };
+        toDatabase(newStock);
+    };
+
+    const toDatabase = async (newStock) => {
+        try {
+            const response = await fetch('http://localhost:8080/User/Add', {
+                method: 'POST',
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify(newStock),
+            });
+
+            console.log(location.state);
+            
+            if (response.ok) {
+                navigate('/RegisterSuccess');
+            } else if (response.status === 409) {
+                const errorMessage = await response.text();
+                alert(errorMessage);
+            } else {
+                const errorData = await response.json();
+                setErrorMessage(errorData.message);
+            }
+        }catch (error) {
+            console.error('NEtwork error:', error);
+        }
+    };
+
     const toReturn = () => {
         navigate('/Register');
     }
+
     return (
         <div className='start-container register'>
             <section className='start-left'>
@@ -18,37 +64,36 @@ export default function Check() {
                 </div>
             </section>
             <section className='start-right'>
-                <form className='register-form'>
+                <form className='register-form' onSubmit={handleSubmit}>
                     <label className='nes'>
                         お名前(アカウント名)
-                        <p className="check-text">○○○</p>
+                        <p className="check-text">{FormData.name}</p>
                     </label>
                     <label className='nes'>
                         メールアドレス(ログインID)
-                        <p className="check-text">○○○</p>
+                        <p className="check-text">{FormData.mailaddress}</p>
                     </label>
                     <label className='nes'>
                         パスワード
-                        <p className="check-text">○○○</p>
+                        <p className="check-text">{FormData.password}</p>
                     </label>
                     <label>性別
-                    <p className="check-text">○○○</p>
+                    <p className="check-text">{FormData.gender}</p>
                     </label>
                     <label>
                         年代
-                        <p className="check-text">○○○</p>
+                        <p className="check-text">{FormData.generation}</p>
                     </label>
                     <label>
                         地域<br />
-                        <p className="check-text">○○○</p>
+                        <p className="check-text">{FormData.region}</p>
                     </label>
                     <div className="form-area">
                     <button className='tocheck-button return-button' onClick={toReturn}>戻る</button>
-                    <button className='tocheck-button' onClick={toDatabase}>完了</button>
+                    <button className='tocheck-button' onClick={toDatabase} type="submit">完了</button>
                     </div>
                 </form>
             </section>
         </div>
-    )
-    
+    );   
 }
