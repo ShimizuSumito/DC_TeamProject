9d2fdcfaf26ea1ec51316bb9e60d5da68196490d
DESKTOP-RG5N8PA\ryo.morisaki <ryo.morisaki@g.dreamcareer.co.jp>
2024-05-27 11:55:07 +0900

    完了画面の作成途中

--- React/src/components/Start/RegisterSuccess/RegisterSuccess.jsx
+++ React/src/components/Start/RegisterSuccess/RegisterSuccess.jsx
@@ -1,7 +1,18 @@
+import './RegisterSuccess.css';
+import { useNavigate } from "react-router-dom";
+
+
 export default function RegisterSuccess() {
+        const navigate = useNavigate();
+        const toDatabase = () => {
+            navigate('/Home');
+        }
     return(
-        <div>
-            <h1>RegisterSuccess</h1>
+        <div className='RegisterSuccess'>
+            <h2>Name + さん</h2>
+            <h1>Cloth Up!へようこそ！！</h1>
+            <p>その日の気分や場面、お出かけ先の天気など様々な条件に合った<br />コーディネートを見つけよう！</p>
+            <button className="">さっそく使ってみる！</button>
         </div>
     )
 }
