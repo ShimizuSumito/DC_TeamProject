0e8d54ab822a0a7ffe227641ee30d6b560f1c576
Takumi3939 <takumi.sakurai@g.dreamcareer.co.jp>
2024-05-28 18:51:39 +0900

    28終了

--- SpringBoot/DC_TeamProject/src/main/java/com/example/demo/Data/UserDatabase.java
+++ SpringBoot/DC_TeamProject/src/main/java/com/example/demo/Data/UserDatabase.java
@@ -4,8 +4,6 @@ import com.example.demo.Interface.UserDatabaseInterface;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
